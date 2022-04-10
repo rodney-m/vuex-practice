081a8567cc60f1e7694ee156aeac7a536e0f4572
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-04-11 00:28:01 +0200

    vuex setup

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <base-container title="Vuex">
+    <h3>{{$store.state.counter}}</h3>
+    <button @click="addOne">Add   1</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+  computed(){
+    return this.$store.state.counter
+  },
+  methods: {
+    addOne(){
+      this.$store.state.counter++
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
